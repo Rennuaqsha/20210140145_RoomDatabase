2127e21e4345928a47528ecf412eea25fabecde9
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2023-12-06 20:08:37 +0800

    added FormInputSiswa function in HalamanEntry

--- app/src/main/java/com/example/praktikum10/ui/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum10/ui/HalamanEntry.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -42,7 +43,23 @@ fun EntrySiswaBody(
             Text(stringResource(id = R.string.btn_submit))
 
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+
 
     }
-
 }
