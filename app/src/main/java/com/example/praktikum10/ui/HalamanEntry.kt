a0a9888a6b4cca89e94fdd44dd507da0e82d84fd
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2023-12-06 20:11:32 +0800

    added outlined text field on FormInputSiswa

--- app/src/main/java/com/example/praktikum10/ui/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum10/ui/HalamanEntry.kt
@@ -6,14 +6,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import com.example.praktikum10.R
 import com.example.praktikum10.navigasi.DestinasiNavigasi
 
@@ -59,7 +62,30 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ) {
-
-
+        OutlinedTextField(
+            value = detailSiswa.nama ,
+            onValueChange = {onValueChange(detailSiswa.copy(nama=it))} ,
+            label = { Text(stringResource(R.string.nama))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.alamat ,
+            onValueChange = {onValueChange(detailSiswa.copy(alamat=it))} ,
+            label = { Text(stringResource(R.string.alamat))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailSiswa.telpon ,
+            onValueChange = {onValueChange(detailSiswa.copy(telpon =it))} ,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(stringResource(R.string.telpon))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
     }
 }
