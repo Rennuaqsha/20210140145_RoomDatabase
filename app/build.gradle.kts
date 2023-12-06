a1aaea6fac3b670d7689a3909689d1cc12560835
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2023-12-06 14:49:14 +0800

    add EntryViewModel.kt file and add viewmodel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,7 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 }
