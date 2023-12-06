c227f7a4b972d7e2398b668297ebd13ddcf0b325
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2023-12-06 20:15:13 +0800

    added form input on EntrySiswaBody

--- app/src/main/java/com/example/praktikum10/ui/HalamanEntry.kt
+++ app/src/main/java/com/example/praktikum10/ui/HalamanEntry.kt
@@ -37,6 +37,11 @@ fun EntrySiswaBody(
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
         modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
     ) {
+        FormInputSiswa(
+            detailSiswa = uiStateSiswa.detailSiswa,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
 
         Button(
             onClick = onSaveClick,
