3ffa0450894e3c51fded7508752107716083e4ab
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2023-12-06 15:33:37 +0800

    add data in file PenyediaViewModel.kt

--- app/src/main/java/com/example/praktikum10/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum10/model/PenyediaViewModel.kt
@@ -1,2 +1,26 @@
 package com.example.praktikum10.model
 
+import EntryViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.praktikum10.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory =  viewModelFactory {
+        initializer {
+            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/*
+Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
+[AplikasiSiswa]
+ */
+fun CreationExtras.AplikasiSiswa(): AplikasiSiswa = (this
+    [ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
